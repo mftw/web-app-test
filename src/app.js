7465a90cc14bfa4b0826947468d0f46b4f60d00f
mftw <michaelnlkoch@gmail.com>
2018-10-05 09:45:06 +0200

    app

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@
  */
 if('serviceWorker' in navigator) {
     window.addEventListener("load", function() {
-        navigator.serviceWorker.register('/sw.js')
+        navigator.serviceWorker.register('./sw.js')
             .then(function (registration) {
                 console.log("Service Worker registred");
             })
